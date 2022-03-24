c924402ff568c8d30db595cc459cef7534455ae1
Lawrence Bensaid <lawrencebensaid@icloud.com>
2022-03-24 17:13:34 -0400

    Rename

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WickedRandom",
-            targets: ["String"]),
+            targets: ["WickedRandom"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
             name: "WickedRandom",
             dependencies: []),
         .testTarget(
-            name: "StringTests",
+            name: "WickedRandomTests",
             dependencies: ["WickedRandom"]),
     ]
 )
